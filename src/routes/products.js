31c51f25ee8137625b9bba92c22850c6d70fa7b7
Sofia Aguilera <aguilerasofia.dv@gmail.com>
2022-10-02 20:25:18 -0300

    middleware3

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,12 +1,14 @@
 const { Router } = require('express')
 const routerProducts = Router()
+const { middleware3 } = require('../middlewares')
+
 
 
 routerProducts.get('/', (req,res) => {
     res.json({msg: 'Get products'})
 })
 
-routerProducts.post('/', (req,res) => {
+routerProducts.post('/', middleware3, (req,res) => {
     res.json({msg: 'Post products'})
 })
 
